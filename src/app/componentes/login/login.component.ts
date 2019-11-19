e16e7e3e41c46dfa95202ff4c6bb8dd020e992b5
rseveric <rseveric@pros.com>
2019-11-18 21:18:13 -0800

    issue/010: login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from 'src/app/servicios/login/login.service';
+import { UserResponse } from 'src/app/modelos/UserResponse';
+import { User } from 'src/app/modelos/User';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +11,29 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router:Router){}
+  constructor(private router:Router, private service:LoginService){}
+
+  arrayUsers:User[];
+  userResponse:UserResponse;
+  message="user or password incorrect";
 
   ngOnInit() {
   }
 
-  navigateDashboard(){
-    this.router.navigate(["nav-bar"]);
+  navigateDashboard(username, password){
+    this.service.getUserById(username).subscribe(
+      response => {
+        this.userResponse = response;
+        this.arrayUsers = this.userResponse.data;
+        for (let user of this.arrayUsers) {
+          if(user.id==username &&  user.password==password){
+            this.router.navigate(["nav-bar"]);
+          } else {
+            alert(this.message);
+          }
+        }
+      },
+      error => alert(this.message)
+    );
   }
-
 }
---
+++ src/app/servicios/login/login.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { UserResponse } from 'src/app/modelos/UserResponse';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http:HttpClient) { }
+  
+  Url=environment.apiUrl + '/api/v1/users/'
+  
+  getUserById(id){
+    this.Url = this.Url + id;
+    return this.http.get<UserResponse>(this.Url)
+  }
+}

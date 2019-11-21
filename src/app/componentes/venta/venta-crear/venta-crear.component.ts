ffe1abd53a541866661a1a348e3b571b519f5c84
rseveric <rseveric@pros.com>
2019-11-21 07:12:38 -0800

    issue/010: Add create venta

--- src/app/componentes/venta/venta-crear/venta-crear.component.ts
+++ src/app/componentes/venta/venta-crear/venta-crear.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { VentaResponse } from 'src/app/modelos/VentaResponse';
+import { Venta } from 'src/app/modelos/Venta';
+import { VentaCrearService } from 'src/app/servicios/venta/venta-crear.service';
 
 @Component({
   selector: 'app-venta-crear',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VentaCrearComponent implements OnInit {
 
-  constructor() { }
+  venta:Venta;
+  ventaResponse:VentaResponse;
+  errorValidation="Error, validation error";
+  errorMessage="Error, please contact your administrator";
+
+  constructor(private router:Router, private service:VentaCrearService) { }
 
   ngOnInit() {
+  }
+
+  ventaCrear(idSeller, idClient, date){
+    this.venta = new Venta();
+    this.venta.idSeller = idSeller;
+    this.venta.idClient = idClient;
+    this.venta.date = date;
+    this.service.createVenta(this.venta).subscribe(
+      data=> {
+        console.log(data);
+        alert("Se creo con exito");
+        this.router.navigate(["venta-mostrar"])
+      },
+      error => {
+        console.log(error);
+        this.ventaResponse = error.error;
+        if (this.ventaResponse.textMessage != undefined){
+          console.log(this.ventaResponse.textMessage);
+          alert(this.ventaResponse.textMessage)
+        } else if (error.error.errors != undefined){
+          let validationMessage = this.errorValidation + "\n";
+          for (let x=0;x<error.error.errors.length;x++){
+            validationMessage = validationMessage + error.error.errors[x].field + " ";
+            validationMessage = validationMessage + error.error.errors[x].defaultMessage + "\n";
+          }
+          alert(validationMessage)
+        } else {
+          alert(this.errorMessage)
+        }
+      }
+    )
+  }
+
+  ventaCancelar(){
+    this.router.navigate(["venta-mostrar"])
   }
 
 }
--- src/app/servicios/venta/venta-crear.service.ts
+++ src/app/servicios/venta/venta-crear.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { VentaResponse } from 'src/app/modelos/VentaResponse';
+import { Venta } from 'src/app/modelos/Venta';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class VentaCrearService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  Url=environment.apiUrl + '/api/v1/sales'
+
+  createVenta(venta:Venta){
+      return this.http.post<VentaResponse>(this.Url, venta)
+  }
 }
